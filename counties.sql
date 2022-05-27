24a055276d59fbcf603ccb41559485893ef08034
spelosi92 <106380500+spelosi92@users.noreply.github.com>
2022-05-27 11:23:57 +0100

    Add files via upload

---
+++ counties.sql
@@ -0,0 +1,18 @@
+CREATE TABLE counties(
+   ID     INTEGER  NOT NULL PRIMARY KEY 
+  ,City   VARCHAR(10)
+  ,County VARCHAR(18)
+);
+INSERT INTO counties(ID,City,County) VALUES (1,'Lincoln','Lincolnshire');
+INSERT INTO counties(ID,City,County) VALUES (2,'Liverpool','Lancashire');
+INSERT INTO counties(ID,City,County) VALUES (3,'London','Greater London');
+INSERT INTO counties(ID,City,County) VALUES (4,'Swansea','Swansea');
+INSERT INTO counties(ID,City,County) VALUES (5,'Plymouth','Devon');
+INSERT INTO counties(ID,City,County) VALUES (6,'Cardiff','Cardiff');
+INSERT INTO counties(ID,City,County) VALUES (7,'Birmingham','West Midlands');
+INSERT INTO counties(ID,City,County) VALUES (8,'Bristol','Bristol');
+INSERT INTO counties(ID,City,County) VALUES (9,'Manchester','Greater Manchester');
+INSERT INTO counties(ID,City,County) VALUES (10,'Aberdeen','Aberdeenshire');
+INSERT INTO counties(ID,City,County) VALUES (11,'Belfast','County Atrim/Down');
+INSERT INTO counties(ID,City,County) VALUES (12,'Newcastle','Northumberland');
+INSERT INTO counties(ID,City,County) VALUES (14,'Brighton','East Sussex');
